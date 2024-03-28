5aaa8090a87d13697af65de61e0f52d6f2a6d32d
Melody Zhao <no30131@gmail.com>
2024-03-28 13:07:18 +0800

    add Assignment-3

---
+++ Week-1/Assignment-3/countAandB.js
@@ -0,0 +1,24 @@
+function countAandB(input) {
+    return input.reduce((accumulator, currentValue) => {
+        if (currentValue === 'a' || currentValue === 'b') {
+            accumulator = accumulator + 1;
+        }
+        return accumulator;
+    }, 0); 
+}
+    
+function toNumber(input) {
+    const toNumberMap = {
+        'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5
+    };
+
+    return input.map(char => toNumberMap[char] || char);
+}
+    
+let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
+console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters and 1 ‘b’ letter)
+console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1, 3]
+    
+let input2 = ['e', 'd', 'c', 'd', 'e'];
+console.log(countAandB(input2)); // should print 0
+console.log(toNumber(input2)); // should print [5, 4, 3, 4, 5]
