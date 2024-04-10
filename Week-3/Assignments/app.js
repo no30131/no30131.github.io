d685f2040f0b3134f4a65dfe632c0bdc3e584d54
Melody Zhao <no30131@gmail.com>
2024-04-10 13:53:30 +0800

    Assignment-2 V1

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -1,11 +1,37 @@
-const express = require('express');
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello! Vegan World!!!');
+app.use("/", router);
+app.set("view engine", "pug");
+
+router.use(cookieParser());
+router.use(bodyParser.urlencoded({ extended: false }));
+
+// Assignment 1
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.listen(port, () => {
-    console.log(`on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
+
+// Assignment 2
+router.get('/data', (req, res) => {
+    res.render('data');
+});
+// router.get("/data", (req, res) => {
+//   // 讀取 URL 查詢參數
+//   const number = req.query.number || "";
+//   res.render("data", { number: number });
+// });
+
+router.post("/data", (req, res) => {
+  res.render("data", { name: req.body.inputnumber });
+});
+
+module.exports = router;
