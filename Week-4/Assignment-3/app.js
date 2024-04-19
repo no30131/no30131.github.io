f32a5f7ceee56234ece633d61017d5ee68ad90cb
Melody Zhao <no30131@gmail.com>
2024-04-19 12:51:04 +0800

    finished home and member page

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -1,46 +1,128 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
-const mysql = require("mysql2");
+let userName = "";
 
 app.use("/", router);
 app.set("view engine", "pug");
-router.use(express.static('public'));
+router.use(express.static("public"));
+router.use(bodyParser.urlencoded({ extended: false }));
 
-// const pool = mysql
-//   .createPool({
-//     host: "127.0.0.1",
-//     user: "root",
-//     password: "sS123456",
-//     database: "assignment",
-//   })
-//   .promise();
+// Database
+const pool = mysql
+  .createPool({
+    host: "127.0.0.1",
+    user: "root",
+    password: "sS123456",
+    database: "assignment",
+  })
+  .promise();
 
-// pool.getConnection((err, connection) => {
-//   if (err) {
-//     console.error("Error connecting to database: ", err);
-//     return;
-//   }
-//   connection.release();
-// });
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error connecting to database: ", err);
+    return;
+  }
+  connection.release();
+});
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.post("/signup", async (req, res) => {
+  console.log("post to signup");
+  const { email, username, password } = req.body;
+
+  // 若 username 或 password 是空白，要求重新輸入
+  if (!username.trim() || !password.trim()) {
+    const errorMessage =
+      "Username and password cannot be empty. Please try again!";
+    return res.render("index.pug", { errorMessage: errorMessage });
+  }
+
+  try {
+    // 檢查 email 是否已存在資料庫
+    const [rows] = await pool.query("SELECT email FROM user WHERE email = ?", [
+      email,
+    ]);
+    if (rows.length > 0) {
+      // 若已存在，要求重新輸入
+      const errorMessage =
+        "This email is already registered, please try a different email.";
+      return res.render("index.pug", { errorMessage: errorMessage });
+    } else {
+      const [rows] = await pool.query(
+        "INSERT INTO user (email, username, password) VALUES (?, ?, ?)",
+        [email, username, password]
+      );
+      userName = username;
+      res.redirect("/member");
+    }
+  } catch (error) {
+    console.error("Error fetching user: ", error);
+    res.status(500).send("internal Server Error");
+  }
+});
+
+app.post("/signin", async (req, res) => {
+  console.log("post to signin");
+  const { email, username, password } = req.body;
+
+  // 若 username 或 password 是空白，要求重新輸入
+  if (!username.trim() || !password.trim()) {
+    const errorMessage =
+      "Username and password cannot be empty. Please try again!";
+    return res.render("index.pug", { errorMessage: errorMessage });
+  }
+
+  try {
+    // 檢查 email 是否已存在資料庫
+    const [erows] = await pool.query("SELECT email FROM user WHERE email = ?", [
+      email,
+    ]);
+    if (erows.length > 0) {
+      // 若已存在，檢查 username, password 是否正確
+      const [rows] = await pool.query(
+        "SELECT email, username, password FROM user WHERE email = ? AND username = ? AND password = ?",
+        [email, username, password]
+      );
+      console.log(rows);
+      if (rows.length > 0) {
+        // 成功登入，記住 username 並重導向
+        userName = username;
+        res.redirect("/member");
+      } else {
+        const errorMessage =
+          "username or password not correct, please try again!";
+        return res.render("index.pug", { errorMessage: errorMessage });
+      }
+    } else {
+      // 若不存在，請使用者去註冊
+      const errorMessage =
+        "This email has not been registered yet, please sign up.";
+      return res.render("index.pug", { errorMessage: errorMessage });
+    }
+  } catch (error) {
+    console.error(error);
+    console.log("Server error");
+    res.status(500).send("Server error");
+  }
+});
+
+app.get("/member", (req, res) => {
+  // 檢查如果已登入才顯示 member 頁面，否則彈回登入頁
+  if (userName !== "") {
+    res.render("member.pug", { name: userName });
+  } else {
+    res.redirect("/");
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-// async function getUser() {
-//   try {
-//     const [rows] = await pool.query("SELECT * FROM user");
-//     console.log(rows);
-//   } catch (error) {
-//     console.error("Error fetching users: ", error);
-//   }
-// }
-
-// getUser();
